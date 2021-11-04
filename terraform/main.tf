d29d62b0173302bf74700b4cc1fac1bd967c7fbe
implodingduck <zsek320@gmail.com>
2021-11-04 10:39:44 -0500

    creating a standard logic app

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.82.0"
+      version = "=2.83.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -58,4 +58,25 @@ resource "azurerm_storage_account" "sa" {
   account_replication_type = "LRS"
 
   tags = local.tags
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = "asp-${local.func_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  kind                = "elastic"
+  reserved = true
+  sku {
+    tier = "WorkflowStandard"
+    size = "WS1"
+  }
+}
+
+resource "azurerm_logic_app_standard" "example" {
+  name                       = "la-${local.func_name}"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.asp.id
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
 }
