a75b74767cfd3918bb1a28f6a5f6a4003f0668fb
implodingduck <zsek320@gmail.com>
2021-11-04 10:48:20 -0500

    ok lets try this

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,8 +65,7 @@ resource "azurerm_app_service_plan" "asp" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   kind                = "elastic"
-  reserved = true
-  sku {
+    sku {
     tier = "WorkflowStandard"
     size = "WS1"
   }
