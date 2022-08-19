d2822d97a88862a0d6dbfc4cc381a62f743cc7cc
implodingduck <zsek320@gmail.com>
2022-08-19 16:40:08 -0500

    reserved is now true

--- terraform/main.tf
+++ terraform/main.tf
@@ -139,7 +139,7 @@ resource "azurerm_app_service_plan" "asp" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   kind                = "elastic"
-  reserved            = false
+  reserved            = true
     sku {
     tier = "WorkflowStandard"
     size = "WS1"
