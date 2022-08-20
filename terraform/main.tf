316a455e862a45ed787e53aea3e6101d31b20e88
implodingduck <zsek320@gmail.com>
2022-08-20 12:42:56 -0500

    disabling public access

--- terraform/main.tf
+++ terraform/main.tf
@@ -158,16 +158,16 @@ resource "azurerm_storage_account" "sa" {
   tags = local.tags
 }
 
-# resource "azurerm_storage_account_network_rules" "fw" {
-#   depends_on = [
-#     azurerm_app_service_virtual_network_swift_connection.example
-#   ]
-#   storage_account_id = azurerm_storage_account.sa.id
+resource "azurerm_storage_account_network_rules" "fw" {
+  depends_on = [
+    azurerm_app_service_virtual_network_swift_connection.example
+  ]
+  storage_account_id = azurerm_storage_account.sa.id
 
-#   default_action = "Deny"
+  default_action = "Deny"
 
-#   virtual_network_subnet_ids = [azurerm_subnet.logicapps.id]
-# }
+  #virtual_network_subnet_ids = [azurerm_subnet.logicapps.id]
+}
 
 resource "azurerm_app_service_plan" "asp" {
   name                = "asp-${local.func_name}"
